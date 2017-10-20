cf062a8f320aa5034636bdda7f64d4aecc646668
astudnev <aleksey@wondersoft.ru>
2017-10-21 00:34:45 +0300

    initial contracts

--- contracts/wallet/Wallet.sol
+++ contracts/wallet/Wallet.sol
@@ -297,7 +297,6 @@ contract multisig {
 
     // FUNCTIONS
 
-    // TODO: document
     function changeOwner(address _from, address _to) external;
     function execute(address _to, uint _value, bytes _data) external returns (bytes32);
     function confirm(bytes32 _h) returns (bool);

309560aed2cc032e5173025a6b980d9d2358017a
astudnev <aleksey@wondersoft.ru>
2017-11-24 18:44:58 +0300

    initial contracts for crowd sale

--- README.md
+++
@@ -1,7 +0,0 @@
-# IZX Smart Contracts
-
-This project contains the smart contracts,
- used to develop applications with IZX tokens.
- 
- 
- 
---
+++ contracts/crowdsale/TokenSale.sol
@@ -0,0 +1,168 @@
+pragma solidity ^0.4.11;
+
+
+import "../token/ControlledToken.sol";
+import "../token/TokenController.sol";
+import "./Owned.sol";
+import './SafeMath.sol';
+
+
+contract TokenSale is TokenController, Owned, SafeMath {
+
+
+    uint public startFundingTime;           // In UNIX Time Format
+    uint public endFundingTime;             // In UNIX Time Format
+    uint public maximumFunding;             // In wei
+    uint public totalCollected;             // In wei
+    ControlledToken public tokenContract;   // The new token for this TokenSale
+    address public vaultAddress;            // The address to hold the funds donated
+    bool public transfersAllowed;           // If the token transfers are allowed
+    uint256 public exchangeRate;            // USD/ETH rate * 100
+    uint public exchangeRateTime;           // Time when exchange rate was set
+
+    /// @notice 'TokenSale()' initiates the TokenSale by setting its funding
+    /// parameters
+    /// @dev There are several checks to make sure the parameters are acceptable
+    /// @param _startFundingTime The UNIX time that the TokenSale will be able to
+    /// start receiving funds
+    /// @param _endFundingTime The UNIX time that the TokenSale will stop being able
+    /// to receive funds
+    /// @param _maximumFunding In wei, the Maximum amount that the TokenSale can
+    /// receive
+    /// @param _vaultAddress The address that will store the donated funds
+    /// @param _tokenAddress Address of the token contract this contract controls
+    /// @param _transfersAllowed if token transfers are allowed
+    function TokenSale (
+        uint _startFundingTime,
+        uint _endFundingTime,
+        uint _maximumFunding,
+        address _vaultAddress,
+        address _tokenAddress,
+        bool _transfersAllowed,
+        uint256 _exchangeRate
+    ) public {
+        require ((_endFundingTime >= now) &&           // Cannot end in the past
+            (_endFundingTime > _startFundingTime) &&
+            (_vaultAddress != 0));                    // To prevent burning ETH
+        startFundingTime = _startFundingTime;
+        endFundingTime = _endFundingTime;
+        maximumFunding = _maximumFunding;
+        tokenContract = ControlledToken(_tokenAddress);// The Deployed Token Contract
+        vaultAddress = _vaultAddress;
+        transfersAllowed = _transfersAllowed;
+        exchangeRate = _exchangeRate;
+        exchangeRateTime = now;
+    }
+
+    /// @dev The fallback function is called when ether is sent to the contract, it
+    /// simply calls `doPayment()` with the address that sent the ether as the
+    /// `_owner`. Payable is a required solidity modifier for functions to receive
+    /// ether, without this modifier functions will throw if ether is sent to them
+    function ()  payable public {
+        doPayment(msg.sender);
+    }
+
+
+    /// @dev `doPayment()` is an internal function that sends the ether that this
+    ///  contract receives to the `vault` and creates tokens in the address of the
+    ///  `_owner` assuming the TokenSale is still accepting funds
+    /// @param _owner The address that will hold the newly created tokens
+
+    function doPayment(address _owner) internal {
+
+        // First check that the TokenSale is allowed to receive this donation
+        require ((now >= startFundingTime) &&
+            (now <= endFundingTime) &&
+            (tokenContract.controller() != 0) &&           // Extra check
+            (msg.value != 0) &&
+            (totalCollected + msg.value <= maximumFunding));
+
+        uint256 tokensAmount = mul(msg.value, exchangeRate) / 100;
+
+        //Track how much the TokenSale has collected
+        totalCollected += msg.value;
+
+        //Send the ether to the vault
+        require (vaultAddress.send(msg.value));
+
+        // Creates an  amount of tokens base on ether sent and exchange rate. The new tokens are created
+        //  in the `_owner` address
+        require (tokenContract.generateTokens(_owner, tokensAmount));
+
+        return;
+    }
+
+    function distributeTokens(address[] _owners, uint256[] _tokens) onlyOwner public {
+
+        require( _owners.length == _tokens.length );
+        for(uint i=0;i<_owners.length;i++){
+            require (tokenContract.generateTokens(_owners[i], _tokens[i]));
+        }
+
+    }
+
+
+    /// @notice `onlyOwner` changes the location that ether is sent
+    /// @param _newVaultAddress The address that will receive the ether sent to this token sale
+    function setVault(address _newVaultAddress) onlyOwner public{
+        vaultAddress = _newVaultAddress;
+    }
+
+    /// @notice `onlyOwner` changes the setting to allow transfer tokens
+    /// @param _allow  allowing to transfer tokens
+    function setTransfersAllowed(bool _allow) onlyOwner public{
+        transfersAllowed = _allow;
+    }
+
+    /// @notice `onlyOwner` changes the exchange rate of token to ETH
+    /// @param _exchangeRate USD/ETH rate * 100
+    function setTransfersAllowed(uint256 _exchangeRate) onlyOwner public{
+        exchangeRate = _exchangeRate;
+        exchangeRateTime = now;
+    }
+
+    /// @notice `onlyOwner` changes the controller of the tokenContract
+    /// @param _newController - controller to be used with token
+    function changeController(address _newController) onlyOwner public {
+        tokenContract.changeController(_newController);
+    }
+
+    /////////////////
+    // TokenController interface
+    /////////////////
+
+    /// @notice `proxyPayment()` allows the caller to send ether to the TokenSale and
+    /// have the tokens created in an address of their choosing
+    /// @param _owner The address that will hold the newly created tokens
+
+    function proxyPayment(address _owner) payable public returns(bool) {
+        doPayment(_owner);
+        return true;
+    }
+
+
+
+    /// @notice Notifies the controller about a transfer, for this TokenSale all
+    ///  transfers are allowed by default and no extra notifications are needed
+    /// @param _from The origin of the transfer
+    /// @param _to The destination of the transfer
+    /// @param _amount The amount of the transfer
+    /// @return False if the controller does not authorize the transfer
+    function onTransfer(address _from, address _to, uint _amount) public returns(bool) {
+        return transfersAllowed;
+    }
+
+    /// @notice Notifies the controller about an approval, for this TokenSale all
+    ///  approvals are allowed by default and no extra notifications are needed
+    /// @param _owner The address that calls `approve()`
+    /// @param _spender The spender in the `approve()` call
+    /// @param _amount The amount in the `approve()` call
+    /// @return False if the controller does not authorize the approval
+    function onApprove(address _owner, address _spender, uint _amount) public
+        returns(bool)
+    {
+        return transfersAllowed;
+    }
+
+
+}

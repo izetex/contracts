d319927b261ac1fb5e5472f8cd6a47f945e167c5
astudnev <aleksey@wondersoft.ru>
2017-12-27 18:43:08 +0300

    scripts added

---
+++ script/execute_multisig.js
@@ -0,0 +1,33 @@
+const cli = require('readline-sync');
+const Connection = require('./eth_connection');
+const wallet = require('./wallet');
+
+const environment = 'foundation'; // ropsten/foundation, change to foundation to deploy to real
+var mnemonics = cli.question('Enter your mnemonics for '+environment+' account:');
+var connection = new Connection(mnemonics, environment);
+
+
+var deployed_wallet = connection.web3.eth.contract(wallet.abi).at(connection.config.vault);
+
+var receiver = cli.question('Enter money recipient:');
+var amount = cli.question('Enter ETH amount:');
+
+var yesno = cli.question('Enter Yes! to continue in '+environment+ ' to '+deployed_wallet.address+' with these parameters: ');
+if(yesno!='Yes!'){
+    console.log('Not confirmed, stopping');
+    process.exit(1);
+}
+console.log('confirming now...');
+
+
+deployed_wallet.execute.sendTransaction(    receiver,
+                                            connection.web3.toWei(amount, 'ether'),
+                                            null, {from: connection.address, gas: 290000},
+    function(error, result){
+        console.log(error, result);
+        if(result) {
+            connection.close();
+            console.log('Done.');
+        }
+    }
+);

63f27dc2b86b840ff3573b49d95c0443574c3e32
astudnev <aleksey@wondersoft.ru>
2017-12-29 18:57:15 +0300

    proxy controller tests

--- test/free_game.js
+++ test/free_game.js
@@ -41,7 +41,7 @@ contract('FreeGameToken', function(accounts) {
 });
 
 
-contract('GameController', function(accounts) {
+contract('ProxyController', function(accounts) {
 
 
     it("should generate tokens to players", function() {
@@ -66,6 +66,89 @@ contract('GameController', function(accounts) {
         }).then(function(result) {
             assert.equal(web3.toWei(10), result.toNumber());
         });
+    });
+
+    it("should allow transfer tokens from player to player", function() {
+        var token;
+
+        return FreeGameToken.deployed().then(function(instance) {
+           token = instance;
+           return token.transfer(accounts[2], web3.toWei(1), {from: accounts[1], gas: 150000});
+        }).then(function() {
+            return token.totalSupply();
+        }).then(function(result) {
+            assert.equal(web3.toWei(15), result.toNumber());
+            return token.balanceOf(accounts[1]);
+        }).then(function(result) {
+            assert.equal(web3.toWei(4), result.toNumber());
+            return token.balanceOf(accounts[2]);
+        }).then(function(result) {
+            assert.equal(web3.toWei(11), result.toNumber());
+        });
+
+
+    });
+
+    it("should allow approve tokens from player to player", function() {
+        var token;
+
+        return FreeGameToken.deployed().then(function(instance) {
+            token = instance;
+            return token.approve(accounts[1], web3.toWei(1), {from: accounts[2], gas: 150000});
+        }).then(function() {
+            return token.allowance(accounts[2], accounts[1]);
+        }).then(function(result) {
+            assert.equal(web3.toWei(1), result.toNumber());
+            return token.transferFrom(accounts[2], accounts[1], web3.toWei(1), {from: accounts[1], gas: 150000});
+        }).then(function() {
+            return token.balanceOf(accounts[1]);
+        }).then(function(result) {
+            assert.equal(web3.toWei(5), result.toNumber());
+            return token.balanceOf(accounts[2]);
+        }).then(function(result) {
+            assert.equal(web3.toWei(10), result.toNumber());
+        });
+
+
+    });
+
+
+    it("should not allow transfer tokens to contract", function() {
+        var token, controller;
+
+        return FreeGameToken.deployed().then(function(instance) {
+            token = instance;
+            return token.controller();
+        }).then(function(result) {
+            controller = ProxyController.at(result);
+            return controller.allowTransfersToContracts();
+        }).then(function(result) {
+            assert.isFalse(result);
+            return token.transfer(token.address, web3.toWei(0.1), {from: accounts[1], gas: 150000});
+        }).then( function(){
+            assert.fail("Exception expected");
+        }).catch(function(error) {
+            return controller.setTransfersToContractsAllowed(true, {from: accounts[0], gas: 150000});
+        }).then( function(){
+            return controller.allowTransfersToContracts();
+        }).then(function(result) {
+            assert.isTrue(result);
+            return token.transfer(token.address, web3.toWei(0.1), {from: accounts[1], gas: 150000});
+        }).then(function() {
+            return token.balanceOf(accounts[1]);
+        }).then(function(result) {
+            assert.equal(web3.toWei(4.9), result.toNumber());
+            return token.balanceOf(token.address);
+        }).then(function(result) {
+            assert.equal(web3.toWei(0.1), result.toNumber());
+            return controller.setTransfersToContractsAllowed(false, {from: accounts[0], gas: 150000});
+        }).then( function(){
+            return controller.allowTransfersToContracts();
+        }).then(function(result) {
+            assert.isFalse(result);
+        });
+
+
     });
 
 });

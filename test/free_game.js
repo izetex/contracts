8d9487f7a75c3284e900e888f233d41b4acb4693
astudnev <aleksey@wondersoft.ru>
2018-01-05 02:49:27 +0300

    test limits GameController

--- test/free_game.js
+++ test/free_game.js
@@ -153,7 +153,7 @@ contract('ProxyController', function(accounts) {
 
 });
 
-contract('FreeGame', function(accounts) {
+contract('GameController', function(accounts) {
 
     it("should accept tokens approval for prizes", function() {
         var token, game, controller;
@@ -165,42 +165,42 @@ contract('FreeGame', function(accounts) {
             return game.controller();
         }).then(function(instance) {
             controller = GameController.at(instance);
-            return controller.approved_amount(accounts[0], game.address);
+            return controller.approved_amount(accounts[2], game.address);
         }).then(function(result) {
             assert.equal(0, result.toNumber());
-            return token.approve(game.address, web3.toWei(2.0), { from: accounts[0], gas: 150000 });
+            return token.approve(game.address, web3.toWei(2.0), { from: accounts[2], gas: 150000 });
         }).then(function() {
-            return token.allowance(accounts[0], game.address);
+            return token.allowance(accounts[2], game.address);
         }).then(function(result) {
             assert.equal(web3.toWei(2.0), result.toNumber());
             return controller.owners(game.address);
         }).then(function(result) {
             assert.equal(1, result.length);
-            assert.equal(accounts[0], result[0]);
-            return controller.owner_index(accounts[0], game.address);
+            assert.equal(accounts[2], result[0]);
+            return controller.owner_index(accounts[2], game.address);
         }).then(function(result) {
             assert.equal(1, result.toNumber());
-            return controller.approved_amount(accounts[0], game.address);
+            return controller.approved_amount(accounts[2], game.address);
         }).then(function(result) {
             assert.equal(web3.toWei(2.0), result.toNumber());
-            return controller.reserved_amount(accounts[0], game.address);
+            return controller.reserved_amount(accounts[2], game.address);
         }).then(function(result) {
             assert.equal(0, result.toNumber());
-            return token.approve(game.address, web3.toWei(3.0), { from: accounts[1], gas: 150000 });
+            return token.approve(game.address, web3.toWei(30.0), { from: accounts[1], gas: 150000 });
         }).then(function() {
             return token.allowance(accounts[1], game.address);
         }).then(function(result) {
-            assert.equal(web3.toWei(3.0), result.toNumber());
+            assert.equal(web3.toWei(30.0), result.toNumber());
             return controller.approved_amount(accounts[1], game.address);
         }).then(function(result) {
-            assert.equal(web3.toWei(3.0), result.toNumber());
+            assert.equal(web3.toWei(30.0), result.toNumber());
             return controller.owner_index(accounts[1], game.address);
         }).then(function(result) {
             assert.equal(2, result.toNumber());
             return controller.owners(game.address);
         }).then(function(result) {
             assert.equal(2, result.length);
-            assert.equal(accounts[0], result[0]);
+            assert.equal(accounts[2], result[0]);
             assert.equal(accounts[1], result[1]);
         });
 
@@ -217,26 +217,93 @@ contract('FreeGame', function(accounts) {
             return game.controller();
         }).then(function(instance) {
             controller = GameController.at(instance);
-            return controller.approved_amount(accounts[0], game.address);
+            return controller.approved_amount(accounts[2], game.address);
         }).then(function(result) {
             assert.equal(web3.toWei(2.0), result.toNumber());
-            return token.approve(game.address, web3.toWei(4.0), { from: accounts[0], gas: 150000 });
+            return token.approve(game.address, web3.toWei(1.0), { from: accounts[2], gas: 150000 });
         }).then(function() {
-            return token.allowance(accounts[0], game.address);
+            return token.allowance(accounts[2], game.address);
         }).then(function(result) {
-            assert.equal(web3.toWei(4.0), result.toNumber());
+            assert.equal(web3.toWei(1.0), result.toNumber());
             return controller.owners(game.address);
         }).then(function(result) {
             assert.equal(2, result.length);
-            assert.equal(accounts[0], result[0]);
+            assert.equal(accounts[2], result[0]);
             assert.equal(accounts[1], result[1]);
-            return controller.owner_index(accounts[0], game.address);
+            return controller.owner_index(accounts[2], game.address);
         }).then(function(result) {
             assert.equal(1, result.toNumber());
         });
 
     });
 
+
+    it("should allow generate tokens to players throw proxy", function() {
+        var token, controller;
+
+        return FreeGameToken.deployed().then(function(instance) {
+            token = instance;
+            return token.controller();
+        }).then(function(result) {
+            controller = ProxyController.at(result);
+            return controller.generateTokens(accounts[1], web3.toWei(5), {from: accounts[0], gas: 150000});
+        }).then(function() {
+            return controller.generateTokens(accounts[2], web3.toWei(10), {from: accounts[0], gas: 150000});
+        }).then(function() {
+            return token.totalSupply();
+        }).then(function(result) {
+            assert.equal(web3.toWei(15), result.toNumber());
+            return token.balanceOf(accounts[1]);
+        }).then(function(result) {
+            assert.equal(web3.toWei(5), result.toNumber());
+            return token.balanceOf(accounts[2]);
+        }).then(function(result) {
+            assert.equal(web3.toWei(10), result.toNumber());
+        });
+    });
+
+    it("should find amount_owner", function() {
+        var token, game, controller;
+        return FreeGameToken.deployed().then(function(instance) {
+            token = instance;
+            return FreeGame.deployed();
+        }).then(function(instance) {
+            game = instance;
+            return game.controller();
+        }).then(function(instance) {
+            controller = GameController.at(instance);
+            return controller.amount_owner(game.address, web3.toWei(0.0));
+        }).then(function(result) {
+            assert.equal(accounts[2], result);
+            return controller.amount_owner(game.address, web3.toWei(1.0));
+        }).then(function(result) {
+            assert.equal(accounts[2], result);
+            return controller.amount_owner(game.address, web3.toWei(3.0));
+        }).then(function(result) {
+            assert.equal(accounts[1], result);
+            return controller.amount_owner(game.address, web3.toWei(5.0));
+        }).then(function(result) {
+            assert.equal(accounts[1], result);
+        });
+
+    });
+
+    it("should limit amount_owner by balance", function() {
+        var token, game, controller;
+        return FreeGameToken.deployed().then(function(instance) {
+            token = instance;
+            return FreeGame.deployed();
+        }).then(function(instance) {
+            game = instance;
+            return game.controller();
+        }).then(function(instance) {
+            controller = GameController.at(instance);
+            return controller.amount_owner(game.address, web3.toWei(5.1));
+        }).then(function(result) {
+            assert.equal(0, result);
+        });
+
+    });
 
 });
 

7879eaee85b92afef13366182fc6558c043c8c05
astudnev <aleksey@wondersoft.ru>
2018-02-12 15:22:13 +0300

    unification for ERC721

--- contracts/drive/CampaignManager.sol
+++ contracts/drive/CampaignManager.sol
@@ -7,9 +7,8 @@ import "../token/IZXToken.sol";
 import 'zeppelin-solidity/contracts/payment/PullPayment.sol';
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
 import 'zeppelin-solidity/contracts/token/ERC721/ERC721.sol';
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract CampaignManager is TokenDriver, PullPayment, Ownable {
+contract CampaignManager is TokenDriver, PullPayment {
 
     struct Prize {
         address master;
@@ -22,14 +21,17 @@ contract CampaignManager is TokenDriver, PullPayment, Ownable {
         uint256 info_hash;
     }
 
-    uint256 public MASTER_PAYOUT_SHARE = 10;
-    uint256 public GAME_PAYOUT_SHARE = 20;
-    uint256 public WINNER_PAYOUT_SHARE = 20;
-    uint256 public HOLDER_PAYOUT_SHARE = 50;
+    struct Payout {
+        uint256 master_payout;
+        uint256 game_payout;
+        uint256 winner_payout;
+        uint256 holder_payout;
+        uint256 token_reserve;
+    }
 
-    uint256 public TOKEN_RESERVE_AMOUNT = 1 ether;
 
-    mapping (address => mapping (uint256 => Prize)) internal prizes;
+    mapping (address => mapping (uint256 => Prize)) public prizes;
+    mapping (address => Payout) public game_payout;
 
     using SafeMath for uint256; // TODO: need it? optimize it?
 
@@ -44,14 +46,19 @@ contract CampaignManager is TokenDriver, PullPayment, Ownable {
         require(_tokenId!=0);
         require( prizes[_erc721][_tokenId].master==address(0) );
 
-        address holder = reserve_tokens( TOKEN_RESERVE_AMOUNT, HOLDER_PAYOUT_SHARE.mul(msg.value)/100);
+        Payout storage payout = game_payout[_game];
+        if(payout.token_reserve==0){
+            game_payout[_game] = Payout(10,20,20,50,1 ether);
+        }
+
+        address holder = reserve_tokens( payout.token_reserve, payout.holder_payout.mul(msg.value)/100);
         require(holder != address(0));
 
         prizes[_erc721][_tokenId] = Prize(msg.sender, _game, holder, msg.value, now + _lifetime, _info_hash );
 
     }
 
-    function claim_prize(ERC721 _erc721, uint256 _tokenId) public { // TODO is it safe to call?
+    function claim_prize(ERC721 _erc721, uint256 _tokenId) public {
 
         Prize storage prize = prizes[_erc721][_tokenId];
 
@@ -63,26 +70,27 @@ contract CampaignManager is TokenDriver, PullPayment, Ownable {
 
         _erc721.takeOwnership(_tokenId);
 
-        payout(prize, winner);
+        Payout storage payout_conditions = game_payout[prize.game];
+        payout(prize, payout_conditions, winner);
 
-        release_tokens(prize.holder, TOKEN_RESERVE_AMOUNT);
+        release_tokens(prize.holder, payout_conditions.token_reserve);
         delete prizes[_erc721][_tokenId];
 
     }
 
-    function payout(Prize _prize, address _winner) private {
+    function payout(Prize _prize, Payout _payout, address _winner) private {
         uint256 payout_amount = _prize.value_eth;
         if(payout_amount>0){
 
              if(_prize.expires_at > now){
-                uint256 game_payout = payout_amount.mul(GAME_PAYOUT_SHARE) / 100;
-                uint256 winner_payout = payout_amount.mul(WINNER_PAYOUT_SHARE) / 100;
-                uint256 holder_payout = payout_amount.mul(HOLDER_PAYOUT_SHARE) / 100;
+                uint256 game_amount = payout_amount.mul(_payout.game_payout) / 100;
+                uint256 winner_amount = payout_amount.mul(_payout.winner_payout) / 100;
+                uint256 holder_amount = payout_amount.mul(_payout.holder_payout) / 100;
 
-                asyncSend(_prize.game, game_payout);
-                asyncSend(_winner, winner_payout);
-                asyncSend(_prize.holder, holder_payout);
-                asyncSend(_prize.master, payout_amount.sub(game_payout).sub(winner_payout).sub(holder_payout));
+                asyncSend(_prize.game, game_amount);
+                asyncSend(_winner, winner_amount);
+                asyncSend(_prize.holder, holder_amount);
+                asyncSend(_prize.master, payout_amount.sub(game_amount).sub(winner_amount).sub(holder_amount));
 
              }else{
                 asyncSend(_prize.master, payout_amount);
@@ -91,15 +99,13 @@ contract CampaignManager is TokenDriver, PullPayment, Ownable {
          }
     }
 
-    function change_payouts(uint256 _master, uint256 _game, uint256 _winner, uint256 _holder, uint256 _reserve )
-            onlyOwner public {
-
-            MASTER_PAYOUT_SHARE = _master;
-            GAME_PAYOUT_SHARE = _game;
-            WINNER_PAYOUT_SHARE = _winner;
-            HOLDER_PAYOUT_SHARE = _holder;
-            TOKEN_RESERVE_AMOUNT = _reserve;
-
+    function change_payouts(uint256 _master_payout, uint256 _game_payout, uint256 _winner_payout,
+                                        uint256 _holder_payout, uint256 _reserve ) public {
+        game_payout[msg.sender] = Payout(   _master_payout,
+                                            _game_payout,
+                                            _winner_payout,
+                                            _holder_payout,
+                                            _reserve);
     }
 
 }

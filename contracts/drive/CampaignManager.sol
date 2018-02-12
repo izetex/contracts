cc0d751b882bc197f12067724d8ef0fce6767e08
astudnev <aleksey@wondersoft.ru>
2018-02-12 14:59:46 +0300

    unification for ERC721

--- contracts/drive/CampaignManager.sol
+++ contracts/drive/CampaignManager.sol
@@ -7,14 +7,14 @@ import "../token/IZXToken.sol";
 import 'zeppelin-solidity/contracts/payment/PullPayment.sol';
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
 import 'zeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract CampaignManager is TokenDriver, PullPayment {
+contract CampaignManager is TokenDriver, PullPayment, Ownable {
 
     struct Prize {
         address master;
         address game;
         address holder;
-        address winner;
 
         uint256 value_eth;
         uint256 expires_at;
@@ -25,9 +25,9 @@ contract CampaignManager is TokenDriver, PullPayment {
     uint256 public MASTER_PAYOUT_SHARE = 10;
     uint256 public GAME_PAYOUT_SHARE = 20;
     uint256 public WINNER_PAYOUT_SHARE = 20;
-    uint256 public HOLDER_PAYOUT_SHARE = 50; // TODO setters !
+    uint256 public HOLDER_PAYOUT_SHARE = 50;
 
-    uint256 public TOKEN_RESERVE_AMOUNT = 1 ether; // TODO setter
+    uint256 public TOKEN_RESERVE_AMOUNT = 1 ether;
 
     mapping (address => mapping (uint256 => Prize)) internal prizes;
 
@@ -47,52 +47,76 @@ contract CampaignManager is TokenDriver, PullPayment {
         address holder = reserve_tokens( TOKEN_RESERVE_AMOUNT, HOLDER_PAYOUT_SHARE.mul(msg.value)/100);
         require(holder != address(0));
 
-        prizes[_erc721][_tokenId] = Prize(msg.sender, _game, holder, address(0), msg.value, now + _lifetime, _info_hash );
+        prizes[_erc721][_tokenId] = Prize(msg.sender, _game, holder, msg.value, now + _lifetime, _info_hash );
 
     }
 
-    function claim_prize(ERC721 _erc721, uint256 _tokenId, address _winner) public {
+    function claim_prize(ERC721 _erc721, uint256 _tokenId) public { // TODO is it safe to call?
 
         Prize storage prize = prizes[_erc721][_tokenId];
 
         require(prize.master != address(0));
-        require(prize.expires_at < now);
+        require(prize.expires_at > now);
 
-        payout(prize);
+        address winner = _erc721.ownerOf(_tokenId);
+        require(winner != address(0));
+
+        _erc721.takeOwnership(_tokenId);
+
+        payout(prize, winner);
+
         release_tokens(prize.holder, TOKEN_RESERVE_AMOUNT);
         delete prizes[_erc721][_tokenId];
+
+    }
+
+    function revoke_prize(ERC721 _erc721, uint256 _tokenId) public {
+
+        Prize storage prize = prizes[_erc721][_tokenId];
+        require(prize.master != address(0));
+
+        address winner = _erc721.ownerOf(_tokenId);
+        require(winner != address(0));
+
+        _erc721.takeOwnership(_tokenId);
+
+        payout(prize, winner);
+
+        release_tokens(prize.holder, TOKEN_RESERVE_AMOUNT);
+        delete prizes[_erc721][_tokenId];
+
     }
 
 
-    function payout(Prize _prize) private {
+    function payout(Prize _prize, address _winner) private {
+        uint256 payout_amount = _prize.value_eth;
+        if(payout_amount>0){
 
-        uint256 payout = _prize.value_eth;
+             if(_prize.expires_at > now){
+                uint256 game_payout = payout_amount.mul(GAME_PAYOUT_SHARE) / 100;
+                uint256 winner_payout = payout_amount.mul(WINNER_PAYOUT_SHARE) / 100;
+                uint256 holder_payout = payout_amount.mul(HOLDER_PAYOUT_SHARE) / 100;
 
-        if(payout==0)
-            return;
+                asyncSend(_prize.game, game_payout);
+                asyncSend(_winner, winner_payout);
+                asyncSend(_prize.holder, holder_payout);
+                asyncSend(_prize.master, payout_amount.sub(game_payout).sub(winner_payout).sub(holder_payout));
 
-        if(_prize.winner!=address(0) && _prize.expires_at >= now ){ // TODO do we need this check?
+             }else{
+                asyncSend(_prize.master, payout_amount);
+             }
 
-             uint256 v = payout.mul(GAME_PAYOUT_SHARE) / 100;
-             asyncSend(_prize.game, v);
-             payout = payout.sub(v);
+         }
+    }
 
-             v = payout.mul(WINNER_PAYOUT_SHARE) / 100;
-             asyncSend(_prize.winner, v);
-             payout = payout.sub(v);
+    function change_payouts(uint256 _master, uint256 _game, uint256 _winner, uint256 _holder, uint256 _reserve )
+            onlyOwner public {
 
-             v = payout.mul(HOLDER_PAYOUT_SHARE) / 100;
-             asyncSend(_prize.holder, v);
-
-             asyncSend(_prize.master, payout.sub(v));
-
-        }else{
-
-            asyncSend(_prize.master, payout);
-
-        }
-
-
+            MASTER_PAYOUT_SHARE = _master;
+            GAME_PAYOUT_SHARE = _game;
+            WINNER_PAYOUT_SHARE = _winner;
+            HOLDER_PAYOUT_SHARE = _holder;
+            TOKEN_RESERVE_AMOUNT = _reserve;
 
     }
 

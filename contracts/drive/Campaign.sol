dc4fae99a8d79dff469e1704953e23f9552c38c9
astudnev <aleksey@wondersoft.ru>
2018-03-06 19:44:58 +0300

    Campaign contract compile

--- contracts/drive/Campaign.sol
+++ contracts/drive/Campaign.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'zeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+import '../token/IZXToken.sol';
 
 
 contract Campaign is Ownable {
@@ -13,10 +14,11 @@ contract Campaign is Ownable {
 
     address     public  sponsor;
     address     public  game;
-    address     public  game_token;
-    address     public  izx_token;
+    ERC721      public  game_token;
+    IZXToken    public  izx_token;
 
     uint256     public  finish_at;
+    bool        public  completed;
 
     uint256     public  sponsor_token_amount;
     uint256     public  holder_token_amount;
@@ -34,13 +36,27 @@ contract Campaign is Ownable {
 
     mapping(uint256 => Prize)   public  prizes;
     mapping(address => uint256) public  token_holder_balance;
+    mapping(address => uint256) public  reserved_balance;
+
+
     address[]                   public  token_holders;
     uint256                     public  next_index;
 
 
-    event Sponsored(address indexed campaign, address indexed sponsor, uint256 value);
-    event Prize(address indexed campaign, address indexed token, uint256 indexed tokenId, uint256 value);
-    event Win(address indexed campaign, address indexed token, address indexed winner, uint256 indexed tokenId);
+    event Sponsored( address indexed sponsor, uint256 value);
+    event NewPrize( address indexed token, uint256 indexed tokenId, uint256 value);
+    event Win( address indexed token, address indexed winner, uint256 indexed tokenId);
+
+
+    modifier onlyGame() {
+        require(msg.sender==game);
+        _;
+    }
+
+    modifier onlySponsor() {
+        require(msg.sender==sponsor);
+        _;
+    }
 
     modifier notFinished() {
         require(now <= finish_at);
@@ -48,21 +64,21 @@ contract Campaign is Ownable {
     }
 
     modifier notCompleted() {
-        require(now <= finish_at || sponsor_token_amount >= PRIZE_PRICE);
+        require(now <= finish_at && !completed);
         _;
     }
 
-    modifier onlyCompleted() {
-        require(now > finish_at || sponsor_token_amount < PRIZE_PRICE);
+    modifier ifCompleted() {
+        require(now > finish_at || completed);
         _;
     }
 
-    modifier onlySponsored() {
+    modifier ifSponsored() {
         require( sponsor_token_amount >= PRIZE_PRICE);
         _;
     }
 
-    function Campaign(address _sponsor, address _game, address _game_token, address _izx_token, uint256 finish_at) public {
+    function Campaign(address _sponsor, address _game, ERC721 _game_token, IZXToken _izx_token, uint256 _finish_at) public {
         sponsor = _sponsor;
         game = _game;
         game_token = _game_token;
@@ -83,11 +99,11 @@ contract Campaign is Ownable {
             token_holders[next] = _sender;
         }
 
-        Sponsored(this, address _sender, _token_amount);
+        Sponsored( _sender, _token_amount);
     }
 
-    function registerPrize(uint256 _tokenId) public onlyGame onlySponsored notFinished {
-        require(ownerOf(_tokenId) != address(0));
+    function registerPrize(uint256 _tokenId) public onlyGame ifSponsored notCompleted {
+        require(game_token.ownerOf(_tokenId) != address(0));
 
         uint256 holder_amount = 0;
         address token_holder = token_holders[next_index];
@@ -102,6 +118,7 @@ contract Campaign is Ownable {
 
             token_holder_balance[token_holder] -= holder_amount;
             holder_token_amount -= holder_amount;
+            reserved_balance[token_holder] += holder_amount;
 
             if(token_holder_balance[token_holder]==0){
                 next_index += 1;
@@ -110,29 +127,31 @@ contract Campaign is Ownable {
         }
 
         sponsor_token_amount -= PRIZE_PRICE;
+        reserved_balance[sponsor] += PRIZE_PRICE;
+
         uint256 holder_reward = (PRIZE_PRICE - DEFAULT_WINNER_REWARD - DEFAULT_GAME_REWARD) + holder_amount;
         prizes[_tokenId] = Prize(token_holder, address(0), DEFAULT_WINNER_REWARD, DEFAULT_GAME_REWARD, holder_reward );
 
-        Prize(this, address game_token, _tokenId, PRIZE_PRICE + holder_amount);
+        NewPrize( game_token, _tokenId, PRIZE_PRICE + holder_amount);
     }
 
-    function winPrize(uint256 _tokenId, address _winner) public onlyGame notFinished {
+    function winPrize(uint256 _tokenId, address _winner) public onlyGame notCompleted {
         Prize storage prize = prizes[_tokenId];
         require(prize.winner_reward > 0);
         require(prize.winner == address(0));
 
         prize.winner = _winner;
 
-        Win(this, game_token, winner, _tokenId);
+        Win( game_token, _winner, _tokenId);
     }
 
-    function rewardPrize(uint256 _tokenId) public {
-
-        takeOwnership(_tokenId);
+    function rewardPrize(uint256 _tokenId) public notCompleted {
         Prize storage prize = prizes[_tokenId];
 
         require(prize.winner_reward > 0);
         require(prize.winner != address(0));
+
+        game_token.takeOwnership(_tokenId);
 
         require(izx_token.transfer(game, prize.game_reward));
         require(izx_token.transfer(prize.winner, prize.winner_reward));
@@ -142,15 +161,24 @@ contract Campaign is Ownable {
 
     }
 
-    function withdraw() public onlyCompleted {
+    function withdraw() public ifCompleted {
+
+        uint256 amount;
 
         if(msg.sender == sponsor){
-
+           amount = reserved_balance[sponsor] + sponsor_token_amount;
         }else{
-
-
+           amount = reserved_balance[msg.sender] + token_holder_balance[msg.sender];
         }
 
+        if(amount>0){
+            require(izx_token.transfer(msg.sender, amount));
+        }
+
+    }
+    
+    function complete() public onlySponsor notCompleted {
+        completed = true;
     }
 
 }
--- contracts/drive/CampaignManager.sol
+++ contracts/drive/CampaignManager.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 import "../token/TokenController.sol";
 import "../token/IZXToken.sol";
 import "./Campaign.sol";
@@ -8,22 +9,22 @@ import "./Campaign.sol";
 
 contract CampaignManager is Ownable, TokenController {
 
-    IZXToken        public token;        // token controlled
-
+    IZXToken                 public  token;
+    mapping(address => bool) public  campaigns;
 
     /**
     * @dev Create the controller for token manager.
-    * @param _manager The token manager, which is eligible to receive IZX tokens
+    * @param _token IZX token
     */
     function CampaignManager( IZXToken _token ) public {
         require(_token != address(0));
         token = _token;
     }
 
-    function createCampaign(address _game, address _game_token) public returns address{
-
-
-
+    function createCampaign(address _game, ERC721 _game_token, uint256 _finish_at) public returns(address){
+        address cmp = new Campaign(msg.sender, _game, _game_token, token, _finish_at);
+        campaigns[cmp] = true;
+        return cmp;
     }
 
     /**
@@ -33,6 +34,35 @@ contract CampaignManager is Ownable, TokenController {
     */
     function changeTokenController(address _newController) onlyOwner public {
         token.changeController(_newController);
+    }
+
+
+    /**
+    * @notice Do not accept payments on token
+    */
+    function proxyPayment(address) payable public returns(bool) {
+        return false;
+    }
+
+    /**
+    * @notice Allow to transfer to any account or manager smart contract
+    */
+    function onTransfer(address _sender, address _to, uint _token_amount) public returns(bool) {
+        if(campaigns[_to]){
+            Campaign cmp = Campaign(_to);
+            cmp.acceptTokens(_sender,_token_amount);
+            return true;
+        }else{
+            return !isContract(_to);
+        }
+    }
+
+    /**
+    * @notice  Allow to approve to any account or manager smart contract
+    */
+    function onApprove(address, address, uint) public returns(bool)
+    {
+        return true;
     }
 
 
@@ -48,28 +78,5 @@ contract CampaignManager is Ownable, TokenController {
         }
         return size>0;
     }
-
-    /**
-    * @notice Do not accept payments on token
-    */
-    function proxyPayment(address) payable public returns(bool) {
-        return false;
-    }
-
-    /**
-    * @notice Allow to transfer to any account or manager smart contract
-    */
-    function onTransfer(address, address _to, uint) public returns(bool) {
-        return (_to==address(manager)) || !isContract(_to);
-    }
-
-    /**
-    * @notice  Allow to approve to any account or manager smart contract
-    */
-    function onApprove(address, address _spender, uint) public returns(bool)
-    {
-        return (_spender==address(manager)) || !isContract(_spender);
-    }
-
 
 }

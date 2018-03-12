e6b79db0dfc18e722402a11323934000b96b49b4
astudnev <aleksey@wondersoft.ru>
2018-03-12 09:07:04 +0300

    TokenMarket

--- contracts/drive/TokenMarket.sol
+++ contracts/drive/TokenMarket.sol
@@ -5,10 +5,9 @@ import 'zeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 import '../token/ERC20.sol';
 
 
-contract TokenMarket  {
+contract TokenMarket is Ownable {
 
     struct Deal {
-        uint    tokenId;
         address initialTokenOwner;
 
         mapping(address => uint256) contributions;
@@ -21,18 +20,29 @@ contract TokenMarket  {
     mapping(uint256 => Deal)        public  deals;
     mapping(address => uint256)     public  balances;
 
+    event Contributed( address indexed contributor, address indexed token, uint256 indexed tokenId, uint256 value);
 
     function TokenMarket(){
 
     }
 
-    function createDeal(){
-
+    function createDeal(uint _tokenId){
+        deals[_tokenId] = Deal(  asset_token.ownerOf(_tokenId) );
     }
 
-    function contribute(){
+    function contribute(uint _tokenId){
 
+        require(balances[msg.sender] > 0);
+        Deal storage deal = deals[_tokenId];
+        require(deal.initialTokenOwner != address(0));
+
+        uint256 amount = balances[msg.sender];
+        deal.contributions[msg.sender] += amount;
+        balances[msg.sender] = 0;
+
+        Contributed(msg.sender, asset_token, _tokenId, amount);
     }
+
 
     function closeDeal(){
 
@@ -41,5 +51,19 @@ contract TokenMarket  {
     function withdrawDeal(){
 
     }
+
+    /* --- called from controller ( owner ) --- */
+    function depositedFrom(address _sender, uint _amount) onlyOwner {
+        balances[_sender] += _amount;
+    }
+
+    function contributedFrom(address _sender, uint _amount, uint _tokenId) onlyOwner{
+        Deal storage deal = deals[_tokenId];
+        require(deal.initialTokenOwner != address(0));
+        deal.contributions[_sender] += _amount;
+        Contributed(_sender, asset_token, _tokenId, _amount);
+    }
+
+
 
 }

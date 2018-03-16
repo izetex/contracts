694825f9fb0685f112f9176ef393f2db08f410f5
astudnev <aleksey@wondersoft.ru>
2018-03-16 12:40:32 +0300

    Infura for contract deployment

--- script/config.js
+++ script/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     ropsten: {
-        url: 'http://node1.ph3.izx.io:8545',
+        url: 'https://ropsten.infura.io/aaIrr36iBro3mIVCdWrg ',
         creator: '0xef34280050e8546d8e1d85ec75250149553fb396',
         token: '0xA423267a405dC90C536ec78766d4F36FfDb30fdd',
         proxy: '0x9f8cd6841d1427cd7c438e109b7e671b4f14bb59',
@@ -9,14 +9,14 @@ module.exports = {
         rev_share_game: '0xadf36f8bae6f8e0b30e4a94f57caeee71126c445'
     },
     ropsten_ico: {
-        url: 'http://node1.ph3.izx.io:8545',
+        url: 'https://ropsten.infura.io/aaIrr36iBro3mIVCdWrg ',
         creator: '0xef34280050e8546d8e1d85ec75250149553fb396',
         token: '0xB65AE3E2cb1051916c87f6ce7db55cB30f7108DC',
         vault: '0xAAeAA8810440b57f3d218bA04e467eBD706b69eE',
         tokensale: '0x804f92260895f3fe4693e80046a96d88f7c7e8b0'
     },
     foundation: {
-        url: 'http://info.ph3.izx.io:8545',
+        url: 'https://mainnet.infura.io/aaIrr36iBro3mIVCdWrg ',
         creator: '0xb2556a76da6e5b99e82fc156f8faad2778225161',
         token: '0x2ad180cbaffbc97237f572148fc1b283b68d8861',
         vault: '0xf4702b0918A8a89dfC38459Ce42198834818F26B',

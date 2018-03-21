28662c52170f7a043f8d6aca2c9e91a6407ed664
astudnev <aleksey@wondersoft.ru>
2018-03-21 14:20:41 +0300

    drive token

--- contracts/drive/DriveToken.sol
+++ contracts/drive/DriveToken.sol
@@ -5,16 +5,13 @@ import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 
 /**
- * @title PuzzleToken for CryptoPuzzleGame
+ * @title DriveToken
  *
- * @dev ERC721 token, used in demo game ( CryptoPuzzleGame ) to demonstrate, how
- * IZX reward can be associated with the prize from an arbitrary game with ERC721 token.
- * The token is given to a winner, who guessed the puzzle. After that, it can be exchange to reward
- * using CampaignManager.
+ * @dev ERC721 token with ability to mint and associated arbitrary data
  *
  * @author Aleksey Studnev <studnev@izx.io>
  */
-contract PuzzleToken is ERC721Token, Ownable {
+contract DriveToken is ERC721Token, Ownable {
 
     string public name = 'Puzzle Win';
     string public symbol = 'PUZZLE';
@@ -23,13 +20,16 @@ contract PuzzleToken is ERC721Token, Ownable {
 
     using SafeMath for uint256;
 
+    mapping(uint256 => bytes) public token_data;
+
     /**
     * @dev mint a token ang give it to address
     * @param _to The address to transfer the new minted token
     * @return minted token ID
     */
-    function mint(address _to) onlyOwner public returns(uint256) {
+    function mint(address _to, bytes _data) onlyOwner public returns(uint256) {
         _mint(_to, last_tokenId);
+        token_data[last_tokenId] = _data;
         last_tokenId = last_tokenId.add(1);
     }
 

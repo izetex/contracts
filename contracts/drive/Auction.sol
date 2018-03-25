3a98014c866fc8812c98d54128a1310840cbac39
astudnev <aleksey@wondersoft.ru>
2018-03-25 19:52:30 +0300

    campaign implementation

--- contracts/drive/Auction.sol
+++ contracts/drive/Auction.sol
@@ -2,13 +2,17 @@ pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 
+import '../token/IZXToken.sol';
 import './TokenDriver.sol';
 
 contract Auction  {
 
     TokenDriver public driver;
     ERC721      public erc721token;
-    address     public creator;
+    IZXToken    public  izx;
+    address     public owner;
+
+    uint        public owner_share;
 
     event Opened( address indexed token, address indexed owner, uint256 tokenId);
     event Bid( address indexed token, address indexed bidder, uint256 tokenId, uint256 amount);
@@ -22,11 +26,15 @@ contract Auction  {
         _;
     }
 
-    function Auction( address _creator, ERC721 _token) public {
+    function Auction( address _owner, ERC721 _token, uint _owner_share ) public {
         require(address(_token) != address(0));
+        require(_owner_share <= 100);
+
         driver = TokenDriver(msg.sender);
+        izx = driver.token();
         erc721token = _token;
-        creator = _creator;
+        owner = _owner;
+        owner_share = _owner_share;
     }
 
     function sell( uint _tokenId, uint _start_price, uint _sale_price, uint _finish_at) public {
--- contracts/drive/Campaign.sol
+++ contracts/drive/Campaign.sol
@@ -1,21 +1,35 @@
 pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
+import '../token/IZXToken.sol';
 import './TokenDriver.sol';
 
 contract Campaign {
 
-    TokenDriver public driver;
-    ERC721      public erc721token;
-    address     public creator;
-    uint        public lifetime;
-    uint        public token_price;
+    struct Prize {
+        address advertiser;
+        address winner;
+        uint    expires_at;
+    }
 
-    event AdvertiserApproved( address indexed token, address indexed advsertiser, uint256 amount);
-    event Claimed( address indexed token, address indexed owner, address indexed advsertiser, uint256 tokenId);
-    event Reclaimed( address indexed token, address indexed owner, address indexed advsertiser, uint256 tokenId);
-    event Converted( address indexed token, address indexed owner, address indexed advsertiser, uint256 tokenId);
+    TokenDriver public  driver;
+    ERC721      public  erc721token;
+    IZXToken    public  izx;
+    address     public  owner;
+    uint        public  lifetime;
+    uint        public  token_price;
+    uint        public  winner_payout;
+    uint        public  owner_payout;
+    
+    mapping(uint256 => Prize) public prizes;
+
+    event Claimed( address indexed token, address indexed winner, address indexed advertiser, uint256 tokenId);
+    event Reclaimed( address indexed token, address indexed winner, address indexed advertiser, uint256 tokenId);
+    event Converted( address indexed token, address indexed winner, address indexed advertiser, uint256 tokenId);
+
+    using SafeMath for uint256;
 
     /**
     * @dev Throws if called by any account other than the token driver.
@@ -25,33 +39,71 @@ contract Campaign {
         _;
     }
 
-    function Campaign( address _creator, ERC721 _token, uint _lifetime, uint _token_price) public {
+    function Campaign(  address _owner, 
+                        ERC721 _token, 
+                        uint _lifetime, 
+                        uint _token_price,
+                        uint _owner_share) public {
 
         require(address(_token) != address(0));
         require(_lifetime > 0);
         require(_token_price > 0);
-
+        require(_owner_share <= 100);
+        
         driver = TokenDriver(msg.sender);
+        izx = driver.token();
         erc721token = _token;
-        creator = _creator;
+        owner = _owner;
         lifetime = _lifetime;
         token_price = _token_price;
+
+        owner_payout = _owner_share.mul(token_price) / 100;
+        winner_payout = _token_price.sub(owner_payout);
     }
 
 
     function claim(uint _tokenId, address _advertiser) public {
+        require(msg.sender == erc721token.ownerOf(_tokenId));
+        require(izx.transferFrom(_advertiser, this, token_price));
+
+        erc721token.takeOwnership(_tokenId);
+        prizes[_tokenId] = Prize(_advertiser, msg.sender, now.add(lifetime) );
+
+        Claimed(erc721token, msg.sender, _advertiser, _tokenId);
     }
 
     function reclaim(uint _tokenId) public {
+        Prize storage prize = prizes[_tokenId];
+        require(prize.winner == msg.sender || now > prize.expires_at);
+        address advertiser = prize.advertiser;
+
+        erc721token.transfer(prize.winner, _tokenId);
+        require(izx.transferFrom(this, advertiser, token_price));
+
+        delete prizes[_tokenId];
+
+        Reclaimed( erc721token, msg.sender, advertiser, _tokenId);
     }
 
     function convert(uint _tokenId) public {
+        Prize storage prize = prizes[_tokenId];
+        require(prize.advertiser == msg.sender);
+
+        address winner = prize.winner;
+        erc721token.transfer(prize.advertiser, _tokenId);
+        
+        if(winner_payout>0){
+            require(izx.transferFrom(this, winner, winner_payout));
+        }
+
+        if(owner_payout>0){
+            require(izx.transferFrom(this, winner, owner_payout));
+        }
+
+        delete prizes[_tokenId];
+
+        Converted( erc721token, winner, msg.sender, _tokenId);
     }
 
-    // Methods called by token driver
-
-    function approved(address _approver, uint256 _amount) public onlyDriver returns(bool){
-        return false;
-    }
 
 }

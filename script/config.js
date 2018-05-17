9922454de85c0a2fd0f4d4d5e2d19fac51a31189
daoready <info@daoready.com>
2018-05-17 10:27:39 +0300

    URL space removed

--- script/config.js
+++ script/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     ropsten: {
-        url: 'https://ropsten.infura.io/aaIrr36iBro3mIVCdWrg ',
+        url: 'https://ropsten.infura.io/aaIrr36iBro3mIVCdWrg',
         creator: '0xef34280050e8546d8e1d85ec75250149553fb396',
         token: '0x9f498853826e45eacd6af36022b6eb586df6c5ac',
         game: '0xfb2fb88292b86f25ba70876a8392fa6fb64f45b7',
@@ -10,14 +10,14 @@ module.exports = {
         max_voting_duration: 8640000
     },
     ropsten_ico: {
-        url: 'https://ropsten.infura.io/aaIrr36iBro3mIVCdWrg ',
+        url: 'https://ropsten.infura.io/aaIrr36iBro3mIVCdWrg',
         creator: '0xef34280050e8546d8e1d85ec75250149553fb396',
         token: '0xB65AE3E2cb1051916c87f6ce7db55cB30f7108DC',
         vault: '0xAAeAA8810440b57f3d218bA04e467eBD706b69eE',
         tokensale: '0x804f92260895f3fe4693e80046a96d88f7c7e8b0'
     },
     foundation: {
-        url: 'https://mainnet.infura.io/aaIrr36iBro3mIVCdWrg ',
+        url: 'https://mainnet.infura.io/aaIrr36iBro3mIVCdWrg',
         creator: '0xb2556a76da6e5b99e82fc156f8faad2778225161',
         token: '0x2ad180cbaffbc97237f572148fc1b283b68d8861',
         vault: '0xf4702b0918A8a89dfC38459Ce42198834818F26B',
